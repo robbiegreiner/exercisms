58dcc68115600616dc4ba6b9b9eea219bca4c313
Robbie Greiner <robbieg@gmail.com>
2018-01-28 11:49:29 -0700

    solving scrabble score but need to refactor

--- javascript/scrabble-score/scrabble-score.js
+++ javascript/scrabble-score/scrabble-score.js
@@ -1,23 +1,30 @@
 const score = (word) => {
-  const splitWord = word.toUpperCase().split('');
-  const values = [
-    {1 :["A", "E", "I", "O", "U", "L","N", "R", "S", "T"]},
-    {2: ["D","G"]},
-    {3: ["B", "C", "M", "P" ]},
-    {4: ["F", "H", "V", "W", "Y" ]},
-    {5: ["K"]},
-    {8: ["J", "X"]},
-    {10: ["Q", "Z"]}
-  ]
-
-  const score = 0;
-  Object.keys(values).forEach(value => {
-    
-  })
-
   if(!word){
     return 0;
   }
+
+
+  const splitWord = word.toUpperCase().split('');
+  const values = {
+    1 :["A", "E", "I", "O", "U", "L","N", "R", "S", "T"],
+    2: ["D","G"],
+    3: ["B", "C", "M", "P" ],
+    4: ["F", "H", "V", "W", "Y" ],
+    5: ["K"],
+    8: ["J", "X"],
+    10: ["Q", "Z"]
+  }
+
+  let score = 0;
+  Object.keys(values).forEach(value => {
+    splitWord.forEach(letter => {
+      if(values[value].includes(letter)){
+        score += parseInt(value);
+      }
+    })
+  })
+
+  return score;
 
 
 
